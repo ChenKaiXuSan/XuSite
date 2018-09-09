01d465831d467925c28f3b4f58c88423e258223a
xu <chenkaixusan@gmail.com>
2018-09-09 17:42:22 +0800

    部署文件设置

--- src/XuSite/settings.py
+++ src/XuSite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from django.conf.global_settings import STATIC_ROOT
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-=a+cc+-)v(v9=kyz!7d(gct7awyjr5+9zhqou9fd=0*#03cgd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.105"]
 
 
 # Application definition
@@ -119,4 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/pi/XuSite/blog/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
